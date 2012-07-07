c215c86500c7f770e8370c2fa5e6ce7921d2852e
Detroit HSCC <detroit-hscc-coordinator@bdpa-detroit.org>
2012-07-07 13:54:44 -0400

    changed sql

--- SQL/upsstoredb.sql
+++ SQL/upsstoredb.sql
@@ -1,4 +1,6 @@
-CREATE TABLE Customer
+create database upsstore;
+use upsstore;
+CREATE TABLE upsstore.Customer
 (
     customerNumber int NOT NULL AUTO_INCREMENT,
     duMoCustomerNumber VARCHAR(50),
@@ -9,49 +11,54 @@ CREATE TABLE Customer
     addressTwo VARCHAR(50),
     city VARCHAR(50),
     state VARCHAR(50),
-    zip VARCHAR(50),
+    zip VARCHAR(10),
     country VARCHAR(50),
-    phoneOne VARCHAR(50),
-    phoneTwo VARCHAR(50),
+    phoneOne VARCHAR(25),
+    phoneTwo VARCHAR(25),
     email VARCHAR(50),
-    creditCard VARCHAR(50),
-    expDate VARCHAR(50),
-    securityCode VARCHAR(50),
+    creditCard VARCHAR(16),
+    expDate VARCHAR(7),
+    securityCode VARCHAR(5),
     billAddress VARCHAR(50),
     billAddressTwo VARCHAR(50),
-    billCity VARCHAR(50),
-    billState VARCHAR(50),
-    billZip VARCHAR(50),
+    billCity VARCHAR(25),
+    billState VARCHAR(2),
+    billZip VARCHAR(10),
     billCountry VARCHAR(50),
-    comments VARCHAR(4000)
+    comments VARCHAR(4000),
+    PRIMARY KEY (customerNumber)
 );
 
-CREATE TABLE Quote_Detail
+CREATE TABLE upsstore.Quote_Detail
 (
     quoteID INT NOT NULL AUTO_INCREMENT,
-    customerNumber integer,
+    customerNumber int NOT NULL,
     quoteDate DATE,
-    firstPrintDate DATE,
-    lotNumbers VARCHAR (200),
-    auctionMonth VARCHAR (30),
+    firstPrintDate DATETIME,
+    lotNumbers VARCHAR(200),
+    auctionMonth VARCHAR(10),
     quoteYear INT,
-    trackingNumbers VARCHAR (200),
-    shipDate DATE,
-    shippingCost DECIMAL (2),
-    packingCost DECIMAL (2),
-    printFlag VARCHAR (1),
-    comments VARCHAR (4000)
+    trackingNumbers VARCHAR(200),
+    shipDate DATETIME,
+    shippingCost DECIMAL(2),
+    packingCost DECIMAL(2),
+    printFlag VARCHAR(1),
+    comments VARCHAR(4000),
+    PRIMARY KEY (quoteID),
+    FOREIGN KEY (customerNumber) REFERENCES Customer(customerNumber)
 );
     
-CREATE TABLE Email
+CREATE TABLE upsstore.Email
 (
     emailId INT NOT NULL AUTO_INCREMENT,
-    emailMsg VARCHAR(4000)
+    emailMsg VARCHAR(4000),
+    PRIMARY KEY (emailId)
 );
     
-CREATE TABLE Login
+CREATE TABLE upsstore.User
 (
-    userId INT,
-    userName VARCHAR (25),
-    password VARCHAR (25)
+    userId INT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(25),
+    password VARCHAR(25),
+    PRIMARY KEY (userId)
 );
