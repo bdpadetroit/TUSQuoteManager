fe900c2bc0752da9fcfc618d1b97b5aa935723e2
Cameron Hughes <hughes.cameron3@gmail.com>
2012-07-11 18:23:02 -0400

    Db Classes

--- src/com/upsstore/objects/Customer.java
+++ src/com/upsstore/objects/Customer.java
@@ -1,6 +1,126 @@
 package com.upsstore.objects;
 
+import java.util.Date;
+
 public class Customer {
+	private int customerNumber;
+	private String duMoCustomerNumber;
+	private String lastName;
+	private String firstName;
+	private String companyName; 
+    private String address;
+    private String addressTwo; 
+    private String city;
+    private String state; 
+    private String zip;
+    private String country; 
+    private String phoneOne;
+    private String phoneTwo; 
+    private String email; 
+    private String creditCard; 
+    private String expDate; 
+    private String securityCode; 
+    private String billAddress;
+    private String billAddressTwo; 
+    private String billCity;
+    private String billState; 
+    private String billZip;
+    private String billCountry;
+    private String comments;
 
+    public int getcustomerNumber() {
+    	return customerNumber;
+    }
+    
+	public String getduMoCustomerNumber() {
+		return duMoCustomerNumber;
+	}
+	
+	public String getlastName() {
+		return lastName;
+	}
+	
+	public String getfirstName() {
+		return firstName;
+	}
+	
+	public String getcompanyName() {
+		return companyName;
+	}
+	
+	public String getaddress() {
+		return address;
+	}
+	
+	public String getaddressTwo() {
+		return addressTwo;
+	}
+	
+	public String getcity() {
+		return city;
+	}
+	
+	public String getstate() {
+		return state;
+	}
+	
+	public String getzip() {
+		return zip;
+	}
+	
+	public String getcountry() {
+		return country;
+	}
+	
+	public String getphoneOne() {
+		return phoneOne;
+	}
+	
+	public String getphoneTwo() {
+		return phoneTwo;
+	}
+	
+	public String getemail() {
+		return email;
+	}
+	
+	public String getcreditCard() {
+		return creditCard;
+	}
+	
+	public String getexpDate() {
+		return expDate;
+	}
+	
+	public String getsecurityCode() {
+		return securityCode;
+	}
+	
+	public String getbillAddress() {
+		return billAddress;
+	}
+	
+	public String getbillAddressTwo() {
+		return billAddressTwo;
+	}
+	
+	public String getbillCity() {
+		return billCity;
+	}
+	
+	public String getbillState() {
+		return billState;
+	}
+	
+	public String getbillZip() {
+		return billZip;
+	}
+	
+	public String getbillCountry() {
+		return billCountry;
+	}
+
+	public String getcomments() {
+		return comments;
+	}
 }
-
---
+++ src/com/upsstore/objects/Email.java
@@ -0,0 +1,14 @@
+package com.upsstore.objects;
+
+public class Email {
+	private int emailId;
+	private String emailMsg;
+
+	public int getemailId() {
+		return emailId;
+	}
+	
+	public String getemailMsg() {
+		return emailMsg;
+	}
+}
